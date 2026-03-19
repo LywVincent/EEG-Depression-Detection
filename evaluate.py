56c81c003d355b88ca0b45eabe7c1eb1ff9e6289
LywVincent <2891276875@qq.com>
2026-03-19 19:03:23 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,1 +1,13 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def plot_confusion_matrix(y_true, y_pred):
+    cm = confusion_matrix(y_true, y_pred)
+
+    sns.heatmap(cm, annot=True, fmt='d')
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.savefig("results/confusion_matrix.png")
+    plt.show()
